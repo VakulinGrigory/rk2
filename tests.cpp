714ee56819d80e53c5756dc646640d9a6bb5ffd0
VakulinGrigory <github.vakulingd@mail.ru>
2024-05-19 20:13:30 +0300

    added tests.cpp

---
+++ tests.cpp
@@ -0,0 +1,27 @@
+TEST(VirtualMachineTest, InitialState) {
+    VirtualMachine vm;
+    EXPECT_EQ(vm.GetCpuCount(), 1);
+    EXPECT_EQ(vm.GetRamSize(), 500);
+}
+
+TEST(VirtualMachineTest, ChangeCpuCount) {
+    VirtualMachine vm;
+    vm.ChangeCpuCount(4);
+    EXPECT_EQ(vm.GetCpuCount(), 4);
+}
+
+TEST(VirtualMachineTest, ChangeRamSize) {
+    VirtualMachine vm;
+    vm.ChangeRamSize(2000);
+    EXPECT_EQ(vm.GetRamSize(), 2000);
+}
+
+TEST(VirtualMachineTest, ResetToSnapshot) {
+    VirtualMachine vm;
+    auto snapshot = vm.TakeSnapshot();
+    vm.ChangeCpuCount(4);
+    vm.ChangeRamSize(2000);
+    vm.ResetToSnapshot(snapshot);
+    EXPECT_EQ(vm.GetCpuCount(), 1);
+    EXPECT_EQ(vm.GetRamSize(), 500);
+}

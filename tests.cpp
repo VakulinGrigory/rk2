838ffdbd37be7d4c683e7eace6449bf9d9124cc3
VakulinGrigory <github.vakulingd@mail.ru>
2024-06-03 09:49:34 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -4,7 +4,7 @@
 TEST(VirtualMachineTest, InitialState) {
     VirtualMachine vm;
     EXPECT_EQ(vm.GetCpuCount(), 1);
-    EXPECT_EQ(vm.GetRamSize(), 500);
+    EXPECT_EQ(vm.GetRamSize(), 300);
 }
 
 TEST(VirtualMachineTest, ChangeCpuCount) {
